0b142c0ea57004f37b94c677fa18b5a070df595d
Viviana Angel <lvfandino@misena.edu.co>
2024-07-15 16:23:06 -0500

    styles

---
+++ src/App.jsx
@@ -0,0 +1,36 @@
+import { useState, useEffect } from "react";
+
+function App() {
+  const URI = "https://api.thecatapi.com/v1/images/search?limit=10";
+  const [data, setData] = useState();
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState();
+
+  useEffect(() => {
+    const fetchData = () =>
+      fetch(URI)
+        .then((res) => res.json())
+        .then((data) => setData(data))
+        .catch((err) => setError(err))
+        .finally(() => setIsLoading(false));
+
+    fetchData();
+  }, []);
+
+
+  if(isLoading){
+    return <div>Loading...</div>
+  }
+
+  return (
+      <div className="container">
+        {data?.map((cat) => (
+          <div key={cat.id}>
+            <img src={cat.url} alt="cat" />
+          </div>
+        ))}
+      </div>
+    
+  );
+}
+export default App;
